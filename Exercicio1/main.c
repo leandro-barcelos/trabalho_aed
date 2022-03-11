dd68059934092659d0b0ab3bf7615465e796733f
leandro-barcelos <leandrobarcelos.f@hotmail.com>
2022-03-11 14:52:03 -0300

    Testando

--- Exercicio1/main.c
+++ Exercicio1/main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "pilha.h"
 
 int main()
@@ -10,32 +9,31 @@ int main()
     int x, resto;
 
     while(1){
-    printf("-----Convertor de base numerica-----\n");
-    printf("Para de um numero Decimal para Binario aperte (B) \n");
-    printf("Para de um numero Decimal para Octal aperte (O) \n");
-    printf("Para de um numero Decimal para Hexadecinal aperte (H) \n");
-    printf("Digite uma opcao:");
+    printf("----- Conversor de base numérica -----\n");
+    printf("B - Decimal para Binário\n");
+    printf("O - Decimal para Octal\n");
+    printf("H - Decimal para Hexadecimal\n");
+    printf("Digite uma opção: ");
     scanf(" %c", &op);
     printf("Digite o numero a ser convertido:");
     scanf("%d", &num);
 
-    if(num < 0){
-          printf("Voce digitou um numero negativo!Fim de programa! \n");
+    if (num < 0){
+          printf("Voce digitou um numero negativo! Fim de programa! \n");
           return 0;
     }
 
-    if(op == 'b' || op == 'B'){
+    if (op == 'b' || op == 'B') {
         base = 2;
     }
-
-    else if(op == 'o' || op == 'O'){
+    else if (op == 'o' || op == 'O') {
         base = 8;
     }
-    else if(op == 'h' || op == 'H'){
+    else if (op == 'h' || op == 'H') {
         base = 16;
     }
     else{
-        printf("Essa opcao nao existe! Tente novamente!\n\n");
+        printf("Essa opção nao existe! Tente novamente!\n\n");
         continue;
     }
 
@@ -53,15 +51,14 @@ int main()
       num = num/base;
     }
 
-     printf("O numero convertido eh: ");
-     while( tamanho_pilha(p)!= 0){
+    printf("O numero convertido eh: ");
+    while( tamanho_pilha(p)!= 0){
         pop(p, &x);
-        printf("%d ", x);
+        printf("%d", x);
 
     }
 
     printf("\n\n");
-    }
 
-    return 0;
+    }
 }
